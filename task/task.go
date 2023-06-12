769c42caa3d5a6d5ffdf73bf2611a271b93278f9
Tiago Melo <tiagoharris@gmail.com>
2023-06-12 09:19:47 -0300

    copyright

--- task/task.go
+++ task/task.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2022 Tiago Melo. All rights reserved.
+// Copyright (c) 2023 Tiago Melo. All rights reserved.
 // Use of this source code is governed by the MIT License that can be found in
 // the LICENSE file.
 package task
--- task/task_test.go
+++ task/task_test.go
@@ -1,3 +1,6 @@
+// Copyright (c) 2023 Tiago Melo. All rights reserved.
+// Use of this source code is governed by the MIT License that can be found in
+// the LICENSE file.
 package task
 
 import (
